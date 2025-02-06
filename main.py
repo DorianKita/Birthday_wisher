205ba9b3612930ee9a6142d6bb82148111ac453e
Dorian Kita <dorian.kita90@gmail.com>
2025-02-06 19:49:20 +0100

    tried datetime module

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-import smtplib
+# import smtplib
+#
+# my_email = "abecadlo@gmail.com"
+# password = "venxft233asdv"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email,to_addrs="testing@yahoo.com", msg="Subject: Testing\n\nThis is testing message.")
 
-my_email = "abecadlo@gmail.com"
-password = "venxft233asdv"
+import datetime as dt
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,to_addrs="testing@yahoo.com", msg="Subject: Testing\n\nThis is testing message.")
+date = dt.datetime.now()
+year = date.tzinfo
+# print(year)
+
+date_of_birth = dt.datetime(year=1990, month=1, day=22)
+print(date_of_birth)
+
